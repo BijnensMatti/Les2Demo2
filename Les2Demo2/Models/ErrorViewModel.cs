afb0f5758367b221377222d94f1206a5c5681f95
Matti Bijnens <matti.bijnens@student.howest.be>
2018-02-19 11:02:04 +0100

    Add project files.

---
+++ Les2Demo2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Les2Demo2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
